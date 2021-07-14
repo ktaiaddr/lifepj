d2bf41a5da2f3ec48bf391f256602003e090465c
ktai.addr <ktai.addr@gmail.com>
2021-07-14 23:08:42 +0900

    save

--- react_app/src/RefuelingPage.tsx
+++ react_app/src/RefuelingPage.tsx
@@ -3,7 +3,8 @@ import * as React from "react";
 import axios from "axios";
 import UserHeader from "./user_header";
 import RefuelingSubHeader from "./RefuelingSubHeader";
-import {useEffect, useState} from "react";
+import {cloneElement, useEffect, useState} from "react";
+import RefuelingSearchCondition from "./RefuelingSearchCondition";
 
 // Hello コンポーネントの属性（プロパティ）を定義
 interface HelloProps {}
@@ -22,10 +23,81 @@ const pageNumSelectable = [10,20,50,100]
 
 export default (props:any)=>{
     const [refuelings_data_list,setRefuelings_data_list]:[Array<refuelings>,any] = useState([])
+    const [refuelingsCount,setRefuelingsCount]:any = useState(0)
     const [readed,setReaded]:any = useState(false)
     const [pageLimitSelect,setPageLimitSelect]:[number,any] = useState(10)
     const [pagingNumber,setPagingNumber]:[number,any] = useState(1)
     const [pagingSelectable,setPagingSelectable]:[number[],any] = useState([1])
+    const [searchCondition, setSearchCondition] :any = useState({
+        date_start:'',
+        date_end:'',
+        distance_lower:'',
+        distance_high:'',
+        amount_lower:'',
+        amount_high:'',
+        gas_station:'',
+        memo:'',
+    })
+    const [buttonDisabled,setButtonDisabled]:any = useState(true)
+    const [resetSubmit,setResetSubmit]:any = useState(false)
+    const [enableSarch,setEnableSarch]:any = useState(false)
+
+    function _setSearchCondition(e:React.ChangeEvent<HTMLInputElement>){
+
+        console.log(e.target.name)
+        console.log(e.target.value)
+        const tmp = {...searchCondition}
+        console.log(tmp)
+        tmp[e.target.name] = e.target.value
+        // setSearchCondition(null)
+        setSearchCondition(tmp)
+        console.log(searchCondition)
+        setButtonDisabled(false)
+    }
+    function searchResult()
+    {
+        setButtonDisabled(true)
+        setPagingNumber(1)
+        setEnableSarch(true)
+    }
+    function resetSearch()
+    {
+        setSearchCondition({
+            date_start:'',
+            date_end:'',
+            distance_lower:'',
+            distance_high:'',
+            amount_lower:'',
+            amount_high:'',
+            gas_station:'',
+            memo:'',
+        })
+        setPagingNumber(1)
+        setEnableSarch(true)
+    }
+    useEffect(()=>{
+        if(enableSarch){
+            resetSearchRequest()
+            setEnableSarch(false)
+        }
+    },[enableSarch])
+
+    function resetSearchRequest(){
+        const f2 = async()=>{
+            const result = await f();
+
+            if(result ){
+                setRefuelings_data_list(result.data.list)
+                setRefuelingsCount(result.data.count)
+                setPagingSelectable([...Array(Math.ceil(result.data.count/pageLimitSelect)).keys()].map(i => ++i))
+                setReaded(true)
+            }
+        }
+        f2();
+
+        setButtonDisabled(true)
+    }
+
 
     function changePageNumSelect(e:React.ChangeEvent<HTMLSelectElement>){
         setPageLimitSelect(e.target.value)
@@ -58,7 +130,18 @@ export default (props:any)=>{
         const url = 'http://localhost:9000/api/refuelings'
         const instance = axios.create({ withCredentials: true })
         const result = await instance.get(url,{
-            params:{limit:pageLimitSelect,page:pagingNumber}
+            params:{
+                limit:pageLimitSelect,
+                page:pagingNumber,
+                date_start:searchCondition.date_start||null,
+                date_end:searchCondition.date_end||null,
+                amount_low:searchCondition.amount_lower?parseInt(searchCondition.amount_lower):null,
+                amount_high:searchCondition.amount_high?parseInt(searchCondition.amount_high):null,
+                distance_low:searchCondition.distance_lower?parseInt(searchCondition.distance_lower):null,
+                distance_high:searchCondition.distance_high?parseInt(searchCondition.distance_high):null,
+                gas_station:searchCondition.gas_station || null,
+                memo:searchCondition.memo || null,
+            },
         }).catch(e=> {
             return {data:{result:'fail'}}
         })
@@ -71,6 +154,7 @@ export default (props:any)=>{
 
             if(result && !cleanedUp){
                 setRefuelings_data_list(result.data.list)
+                setRefuelingsCount(result.data.count)
 
                 setPagingSelectable([...Array(Math.ceil(result.data.count/pageLimitSelect)).keys()].map(i => ++i))
                 setReaded(true)
@@ -79,6 +163,8 @@ export default (props:any)=>{
         f2();
         const cleanup = () => { cleanedUp = true; };
         return cleanup;
+        console.log(33333)
+
     }, [pageLimitSelect,pagingNumber]);
 
     return (
@@ -92,6 +178,7 @@ export default (props:any)=>{
             }
             {readed ===true?
                 <div>
+
                     <div className="row">
                         <div className="col-5 col-sm-2">
                             <div className="input-group">
@@ -103,8 +190,7 @@ export default (props:any)=>{
                             </div>
                         </div>
                         <div className="col-7 col-sm-10">
-                        <nav aria-label="Page navigation example">
-
+                            <nav aria-label="Page navigation example">
                                 <ul className="pagination justify-content-end">
                                     <li className="page-item">
                                         <a className="page-link" href="#" onClick={pagingPrevious}>＜</a>
@@ -125,28 +211,38 @@ export default (props:any)=>{
                     </div>
 
 
-                <table className="table">
-                    <thead>
-                    <tr>
-                        <th>日付</th>
-                        <th>距離</th>
-                        <th>数量</th>
-                        <th>燃費</th>
-                        <th>ガスステーション</th>
-                        <th>メモ</th>
-                    </tr>
-                    </thead><tbody>
-                {refuelings_data_list.map((value,index)=>(
-                    <tr key={value.refueling_id} style={index%2==0?{background:"lightblue"}:{}}>
-                        <td>{value.date}</td>
-                        <td>{value.refueling_distance}</td>
-                        <td>{value.refueling_amount}</td>
-                        <td>{value.fuel_economy}</td>
-                        <td>{value.gas_station}</td>
-                        <td>{value.memo}</td>
-                    </tr>
+                    <RefuelingSearchCondition
+                        searchCondition={searchCondition}
+                        _setSearchCondition={_setSearchCondition}
+                        searchResult={searchResult}
+                        resetSearch={resetSearch}
+                        buttonDisabled={buttonDisabled} />
+
+                    <div>{refuelingsCount}件</div>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th>日付</th>
+                            <th>距離</th>
+                            <th>数量</th>
+                            <th>燃費</th>
+                            <th>ガスステーション</th>
+                            <th>メモ</th>
+                        </tr>
+                        </thead><tbody>
+                    {refuelings_data_list.map((value,index)=>(
+                        <tr key={value.refueling_id} style={index%2==0?{background:"lightblue"}:{}}>
+                            <td>{value.date}</td>
+                            <td>{value.refueling_distance}</td>
+                            <td>{value.refueling_amount}</td>
+                            <td>{value.fuel_economy}</td>
+                            <td>{value.gas_station}</td>
+                            <td>{value.memo}</td>
+                        </tr>
                    ))}
-                </tbody></table></div>:
+                    </tbody></table>
+                </div>
+                :
                 (<></>)
             }
         </>
---
+++ react_app/src/RefuelingSearchCondition.tsx
@@ -0,0 +1,134 @@
+import * as React from "react";
+import {useEffect, useState} from "react";
+
+export default (props:any)=> {
+
+    const _setSearchCondition = props._setSearchCondition
+    const searchCondition = props.searchCondition
+    const searchResult = props.searchResult
+    const resetSearch = props.resetSearch
+    const buttonDisabled = props.buttonDisabled
+
+    return (
+        <>
+            <div className="row mb-4">
+                <div className="col-lg-6">
+                    <div className="row">
+                        <div className="col">
+                            日付：
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-5">
+                            <input type="date"
+                                   className="form-control"
+                                   placeholder="日付"
+                                   name="date_start"
+                                   value={searchCondition.date_start}
+                                   onInput={_setSearchCondition} />
+                        </div>
+                        ～
+                        <div className="col-lg-5">
+                            <input type="date"
+                                   value={searchCondition.date_end}
+                                   className="form-control"
+                                   placeholder="日付"
+                                   name="date_end"
+                                   onInput={_setSearchCondition}  />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <div className="row">
+                        <div className="col">
+                            距離：
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-5">
+                            <input type="number"
+                                   className="form-control"
+                                   value={searchCondition.distance_lower}
+                                   placeholder="距離下限"
+                                   min="0" max="1000"
+                                   name="distance_lower"
+                                   onInput={_setSearchCondition} />
+                        </div>
+                        ～
+                        <div className="col-lg-5">
+                            <input type="number"
+                                   value={searchCondition.distance_high}
+                                   className="form-control"
+                                   placeholder="距離上限"
+                                   min="0"
+                                   max="1000"
+                                   name="distance_high"
+                                   onInput={_setSearchCondition} />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-3">
+                    <div className="row">
+                        <div className="col">
+                            数量：
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-5">
+                            <input type="number"
+                                   className="form-control"
+                                   placeholder="数量下限"
+                                   value={searchCondition.amount_lower}
+                                   min="0" max="100"
+                                   name="amount_lower" onChange={_setSearchCondition} />
+                        </div>
+                        ～
+                        <div className="col-lg-5">
+                            <input type="number" className="form-control" placeholder="数量上限" min="0" max="100"
+                                   value={searchCondition.amount_high}
+                                   name="amount_high" onInput={_setSearchCondition} />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className="row mb-4">
+
+                <div className="col-lg-6">
+                    <div className="row">
+                        <div className="col">
+                            ガスステーション：
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <input type="text"
+                                       value={searchCondition.gas_station}
+                                       className="form-control" placeholder="ガスステーション" name="gas_station" onInput={_setSearchCondition} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="row">
+                        <div className="col">
+                            メモ：
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <input type="text"
+                                       value={searchCondition.memo}
+                                       className="form-control" placeholder="メモ" name="memo" onInput={_setSearchCondition} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row mb-4">
+                <div className="col-lg-6">
+                    <button className={"btn"+ (buttonDisabled?" btn-secondary":" btn-primary")} onClick={searchResult} disabled={buttonDisabled}>検索</button>
+                    <button className={"btn"+ (buttonDisabled?" btn-secondary":" btn-primary")} onClick={resetSearch} disabled={buttonDisabled}>リセット</button>
+                </div>
+            </div>
+        </>
+    )
+}
