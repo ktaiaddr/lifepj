7eefc199e50f1a10de1d731a637cf7f345a52288
ktai.addr <ktai.addr@gmail.com>
2021-08-03 22:49:27 +0900

    modelのプロパティを定義

--- laravel_app/app/Models/Refueling.php
+++ laravel_app/app/Models/Refueling.php
@@ -5,6 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Refueling
+ * @property int $refueling_id
+ * @property int $user_id
+ * @property string $date
+ * @property float $refueling_amount
+ * @property float $refueling_distance
+ * @property string $gas_station
+ * @property string $memo
+ * @property string $created_at
+ * @property string $updated_at
+ * @package App\Models
+ */
 class Refueling extends Model
 {
     use HasFactory;
