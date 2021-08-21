5c92820a57379215bd27776039c7e623094f5947
ktai.addr <ktai.addr@gmail.com>
2021-08-21 21:16:09 +0900

    パスワードリセットメール送信時のエラーの対応

--- laravel_app/app/Http/Controllers/MyLogin/PasswordResetController.php
+++ laravel_app/app/Http/Controllers/MyLogin/PasswordResetController.php
@@ -5,20 +5,17 @@ namespace App\Http\Controllers\MyLogin;
 
 
 use App\Http\Controllers\Controller;
+use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Password;
 
 class PasswordResetController extends Controller
 {
 
     /**
-     * Handle an incoming password reset link request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Illuminate\Validation\ValidationException
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function __invoke(Request $request)
     {
@@ -26,12 +23,17 @@ class PasswordResetController extends Controller
             'email' => 'required|email',
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
+        try{
+            // We will send the password reset link to this user. Once we have attempted
+            // to send the link, we will examine the response then see the message we
+            // need to show to the user. Finally, we'll send out a proper response.
+            $status = Password::sendResetLink(
+                $request->only('email')
+            );
+        }catch(Exception $e){
+            Log::error($e->getMessage());
+            $status = null;
+        }
 
         return $status == Password::RESET_LINK_SENT
             ? response()->json(true,200)
